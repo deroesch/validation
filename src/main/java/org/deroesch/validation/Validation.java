a704c23bd9fd8e4a0aca2988d9401ccb23c6098d
XBBLM0L <XBBLM0L@L151-8G8QM13.ams.bnymellon.net>
2021-01-05 10:34:01 -0500

    Initial Commit

---
+++ src/main/java/org/deroesch/validation/Validation.java
@@ -0,0 +1,100 @@
+package org.deroesch.validation;
+
+import java.util.Objects;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+/**
+ * Experiments with arg validation
+ */
+public class Validation implements Cloneable {
+
+    public Validation(@NotNull @PositiveOrZero final Integer integer, @NotNull final String string) {
+        setInteger(integer);
+        setString(string);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    /**
+     * @return the integer
+     */
+    @NotNull
+    @PositiveOrZero
+    public Integer getInteger() {
+        return positiveOrZero(integer);
+    }
+
+    /**
+     * @param integer the integer to set
+     */
+    public void setInteger(@NotNull @PositiveOrZero final Integer integer) {
+        this.integer = positiveOrZero(integer);
+    }
+
+    /**
+     * @return the string
+     */
+    @NotNull
+    public String getString() {
+        return Objects.requireNonNull(string);
+    }
+
+    /**
+     * @param string the string to set
+     */
+    public void setString(@NotNull final String string) {
+        this.string = Objects.requireNonNull(string);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(integer, string);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Validation)) {
+            return false;
+        }
+        final Validation other = (Validation) obj;
+        return Objects.equals(integer, other.integer) && Objects.equals(string, other.string);
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("Validation [integer=");
+        builder.append(integer);
+        builder.append(", string=");
+        builder.append(string);
+        builder.append("]");
+        return builder.toString();
+    }
+
+    /**
+     * Implement NotNull and positiveOrZero for Integers
+     */
+    private Integer positiveOrZero(@NotNull @PositiveOrZero @NotNull final Integer integer) {
+        Objects.requireNonNull(integer);
+        if (integer < 0)
+            throw new IllegalArgumentException("integer must be >= 0");
+        return integer;
+    }
+
+    @NotNull
+    @PositiveOrZero
+    Integer integer;
+
+    @NotNull
+    String string;
+
+}
---
+++ src/test/java/org/deroesch/validation/ValidationTest.java
@@ -0,0 +1,145 @@
+/**
+ *
+ */
+package org.deroesch.validation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Experiments with arg validation
+ */
+class ValidationTest {
+
+    private static final int TEN = 10;
+    private static final String HELLO = "hello";
+    private Validation v;
+
+    @BeforeEach
+    void setup() {
+        v = new Validation(TEN, HELLO);
+    }
+
+    /**
+     * Test method for {@link org.deroesch.validation.Validation#hashCode()}.
+     *
+     * @throws CloneNotSupportedException
+     */
+    @Test
+    void testHashCode() throws CloneNotSupportedException {
+        final Validation v1 = (Validation) v.clone();
+        final Validation v2 = (Validation) v.clone();
+
+        assertEquals(v1.hashCode(), v2.hashCode());
+    }
+
+    /**
+     * Test method for
+     * {@link org.deroesch.validation.Validation#Validation(java.lang.Integer, java.lang.String)}.
+     */
+    @Test
+    void testFieldConstructor() {
+        final int int15 = 15;
+        final String string15 = "Fifteen";
+
+        v = new Validation(int15, string15);
+
+        assertEquals(int15, v.getInteger());
+        assertEquals(string15, v.getString());
+    }
+
+    /**
+     * Test method for {@link org.deroesch.validation.Validation#getInteger()}.
+     */
+    @Test
+    void testGetInteger() {
+        final Integer i = v.getInteger();
+        assertNotNull(i);
+        assertEquals(TEN, i);
+    }
+
+    /**
+     * Test method for
+     * {@link org.deroesch.validation.Validation#setInteger(java.lang.Integer)}.
+     */
+    @Test
+    void testSetInteger() {
+        final int i = 11;
+        v.setInteger(i);
+        assertEquals(i, v.getInteger());
+
+        // Test bad arg
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            v.setInteger(-1);
+        });
+
+        // Test null arg
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            v.setInteger(null);
+        });
+    }
+
+    /**
+     * Test method for {@link org.deroesch.validation.Validation#getString()}.
+     */
+    @Test
+    void testGetString() {
+        final String str = v.getString();
+        assertNotNull(str);
+        assertEquals(HELLO, str);
+    }
+
+    /**
+     * Test method for
+     * {@link org.deroesch.validation.Validation#setString(java.lang.String)}.
+     */
+    @Test
+    void testSetString() {
+        final String there = "there";
+        v.setString(there);
+        assertEquals(there, v.getString());
+
+        // Test null arg
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            v.setInteger(null);
+        });
+    }
+
+    /**
+     * Test method for
+     * {@link org.deroesch.validation.Validation#equals(java.lang.Object)}.
+     *
+     * @throws CloneNotSupportedException
+     */
+    @Test
+    void testEqualsObject() throws CloneNotSupportedException {
+        final Validation v1 = (Validation) v.clone();
+        final Validation v2 = (Validation) v.clone();
+        final Object obj = new Object();
+
+        assertEquals(v1, v2);
+        assertEquals(v2, v1);
+        assertEquals(v2, v2);
+        assertFalse(v1.equals(null));
+        assertFalse(v1.equals(obj));
+        assertFalse(obj.equals(v2));
+        assertFalse(v1.equals(new Validation(20, HELLO)));
+        assertFalse(v1.equals(new Validation(TEN, "Sorry")));
+        assertFalse(v1 == v2);
+    }
+
+    /**
+     * Test method for {@link org.deroesch.validation.Validation#toString()}.
+     */
+    @Test
+    void testToString() {
+        final String s = String.format("Validation [integer=%s, string=%s]", TEN, HELLO);
+        assertEquals(s, v.toString());
+    }
+
+}
